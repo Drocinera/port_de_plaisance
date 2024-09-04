0120770a25d05c91f3b0f5763085deaa22eaa2e2
Drocinera <theopichonneau@orange.fr>
2024-09-04 14:55:21 +0200

    Fixe MongoDB URI et retirer options déprécier

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connecté...'))
   .catch(err => console.error('Erreur de connexion à MongoDB:', err));
 
