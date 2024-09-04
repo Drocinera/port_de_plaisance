05e796961c7da3c84a9f9f42a98346cf3c55df4a
Drocinera <theopichonneau@orange.fr>
2024-09-04 16:08:11 +0200

    fiew change

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ReservationController = require('../controllers/reservationController');
+const ReservationController = require('./controllers/reservationController');
 
 // Routes CRUD pour les réservations
 router.post('/create', ReservationController.createReservation);
