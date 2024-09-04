b14837b0652eaa3841c7e4c32dc96d6332d1a4ce
Drocinera <theopichonneau@orange.fr>
2024-09-04 14:57:46 +0200

    ajout du chemin pour page d'acceuil

--- app.js
+++ app.js
@@ -23,3 +23,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur le port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur la page d\'accueil!');
+});
