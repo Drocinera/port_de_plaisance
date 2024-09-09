e2a0fcd06a14ea8b31da70e0cc922eaf75be48ac
Drocinera <theopichonneau@orange.fr>
2024-09-09 15:53:26 +0200

    Changement auth  et ajout de email dans le formulaire

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,11 +4,11 @@ const bcrypt = require('bcryptjs');
 
 exports.signup = async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const existingUser = await User.findOne({ name });
-    if (existingUser) return res.status(400).json({ message: 'Nom déjà utilisé' });
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ username });
+    if (existingUser) return res.status(400).json({ message: 'Nom d\'utilisateur déjà utilisé' });
 
-    const newUser = new User({ name, email, password: hashedPassword });
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
 
     res.status(201).json({ message: 'Utilisateur créé' });
@@ -19,13 +19,16 @@ exports.signup = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, email, password } = req.body;
 
     const user = await User.findOne({ username });
     if (!user) return res.status(400).json({ message: 'Identifiants incorrects' });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: 'Identifiants incorrects' });
+    if (!isMatch) return res.status(400).json({ message: 'Mot de passe incorrects' });
+
+    const Email = await email.findOne({ email });
+    if (!Email) return res.status(400).json({ message: 'Email incorrect' });
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
