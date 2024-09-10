89116cc7d1a04869444ccf408388f9463bf2b05e
Drocinera <theopichonneau@orange.fr>
2024-09-10 17:35:00 +0200

    reglement des routes update et delete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 
 router.post('/create', userController.createUser);
 router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.put('/udpate:id', userController.updateUser);
+router.delete('/delete:id', userController.deleteUser);
 
 module.exports = router;
