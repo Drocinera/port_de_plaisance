9be6d4e69f494fe8e6d66a3d53864019322d9aab
Drocinera <theopichonneau@orange.fr>
2024-09-11 14:45:56 +0200

    Probleme de user et de route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,15 @@ const userController = require('../controllers/userController');
 
 router.post('/create', userController.createUser);
 router.get('/:id', userController.getUserById);
-router.put('/udpate:id', userController.updateUser);
+exports.updateUser = async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.body.userId, req.body, { new: true, runValidators: true });
+      if (!updatedUser) return res.status(404).json({ message: "Utilisateur non trouvé" });
+      res.json(updatedUser);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
 router.delete('/delete:id', userController.deleteUser);
 
 module.exports = router;
