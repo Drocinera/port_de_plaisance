98f328a107f438662ed2a5a28699f233ebfb3956
Drocinera <theopichonneau@orange.fr>
2024-09-11 15:40:12 +0200

    changement d'erreur lier au route et foncction delete et put

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 
 router.post('/create', userController.createUser);
 router.get('/:id', userController.getUserById);
-router.put('/update', userController.updateUser);
+router.put('/update/:id', userController.updateUser);
 router.delete('/delete/:id', userController.deleteUser);
 
 module.exports = router;
